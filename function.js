9b718ad916417d644eced5a96ec3d69b2e713d8d
wizardofwar9 <103306177+wizardofwar9@users.noreply.github.com>
2022-06-30 15:11:06 +0200

    300622

---
+++ function.js
@@ -0,0 +1,45 @@
+/*function affichage()
+{
+    let data =16;
+
+    function another_function()
+    {
+        data=data+1;
+        document.write(data);
+    }
+    another_function();
+    
+}
+affichage();
+*/
+/*
+let mod = (x,y)=>{return x%y};
+document.write(mod(45,5)); 
+
+    if 
+      (mod = 0)   {
+    document.write(" c'est bon !");
+        }
+       else
+       
+        {document.write("  pas bon ! ");
+    }
+   */
+  /*
+  let i = 0;
+
+  while(i<51)
+  {
+    document.write("je suis une boucle -");
+    i++;
+  }*/
+  /*
+  for(let i = 0 ; i < 5 ; ++ i)
+  {
+    document.write(" giteub ");
+
+    if(i === 5)
+    continue;
+    document.write("titi");
+  }
+*/
