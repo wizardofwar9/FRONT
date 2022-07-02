5ec8427120c0842eae8a86b9e9c7f27dace2c7c7
wizardofwar9 <103306177+wizardofwar9@users.noreply.github.com>
2022-07-02 10:27:43 +0200

    cvgrid0206

---
+++ ALGO/cvbgrid.js
@@ -0,0 +1,6 @@
+document.write(" ");
+document.write(" ");
+document.write("Site visité  à : ")
+var d = new Date();
+var hours = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+document.write(hours);
