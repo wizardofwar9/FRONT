53d02ce5b9f1d17c3f2ed5a4a390016d7fbd6878
wizardofwar9 <103306177+wizardofwar9@users.noreply.github.com>
2022-07-16 23:34:47 +0200

    160722

--- ALGO/T.P/dom.js
+++ ALGO/T.P/dom.js
@@ -1,11 +1,23 @@
 
- var prenom = b1
+ /*var prenom = b1
  var nom = b2
 var result = b1+b2
 
 function myFunction() {
     document.getElementById("b1").value;
     document.getElementById("b2").value;
-    document.getElementById[prenom+nom].innerHTML=("b3");
+    document.getElementById().innerHTML=("b3");
 
+}*/
+
+
+
+
+
+/*
+
+function myFunction() {
+  document.getElementById("b1","b2").value = 'b1+b2';
 }
+*/  f[2].onclick = function ()
+{f.nom_prenom.value = f[0].value +" "+ f[1].value}
