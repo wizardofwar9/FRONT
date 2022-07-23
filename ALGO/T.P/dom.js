2d29950d2257e62565847483addc47da371e3715
wizardofwar9 <103306177+wizardofwar9@users.noreply.github.com>
2022-07-23 06:57:53 +0200

    maison 23

--- ALGO/T.P/dom.js
+++ ALGO/T.P/dom.js
@@ -4,12 +4,15 @@
 var result = b1+b2
 
 function myFunction() {
-    document.getElementById("b1").value;
-    document.getElementById("b2").value;
-    document.getElementById().innerHTML=("b3");
+   var prenom = document.getElementById("b1").value;
+   var nom = document.getElementById("b2").value;
+   var result =  document.getElementById().innerHTML=("b3");
 
 }*/
-
+/*
+select.addEventListener("input", (e) => {
+  language = (e.target.value);
+});*/
 
 
 
@@ -19,5 +22,10 @@ function myFunction() {
 function myFunction() {
   document.getElementById("b1","b2").value = 'b1+b2';
 }
-*/  f[2].onclick = function ()
-{f.nom_prenom.value = f[0].value +" "+ f[1].value}
+*/ /*
+f[2].onclick = function ()
+{f.nom_prenom.value = f[0].value +" "+ f[1].value};*/
+
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
